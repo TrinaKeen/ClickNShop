a717359ed8af9fe3ebc0d541aa2a3e2dc5d17dcb
s-moloo <shiraz.moloo@edu.sait.ca>
2024-07-24 15:20:29 -0600

    Updated to MenPage

--- app/MenPage/product-list.js
+++ app/MenPage/product-list.js
@@ -4,10 +4,10 @@ import { useState } from "react";
 import Product from './product';
 
 export default function ProductList({ products }) {
-    const [sortBy, setSortBy] = useState('name');
+    const [sortBy, setSortBy] = useState('category');
   
     const sortedProducts = [...products].sort((a, b) => {
-      if (sortBy === 'name') {
+      if (sortBy === 'category') {
         return a.name.localeCompare(b.name);
       } else if (sortBy === 'price') {
         return a.price.localeCompare(b.price);
@@ -19,10 +19,22 @@ export default function ProductList({ products }) {
       <div className="p-4 max-w-lg mx-auto text-black">
         <div className="flex justify-center mb-4 text-black">
           <button
-            className={`px-4 py-2 mr-2 rounded ${sortBy === 'name' ? 'bg-orange-500 text-black' : 'bg-gray-200'}`}
-            onClick={() => setSortBy('name')}
+            className={`px-4 py-2 mr-2 rounded ${sortBy === 'category' ? 'bg-orange-500 text-black' : 'bg-gray-200'}`}
+            onClick={() => setSortBy('category')}
           >
             Apparel
+          </button>
+          <button
+            className={`px-4 py-2 mr-2 rounded ${sortBy === 'category' ? 'bg-orange-500 text-black' : 'bg-gray-200'}`}
+            onClick={() => setSortBy('category')}
+          >
+            Shoes
+          </button>
+          <button
+            className={`px-4 py-2 mr-2 rounded ${sortBy === 'category' ? 'bg-orange-500 text-black' : 'bg-gray-200'}`}
+            onClick={() => setSortBy('category')}
+          >
+            Accessories
           </button>
           {/* <button
             className={`px-4 py-2 rounded ${sortBy === 'price' ? 'bg-orange-500 text-black' : 'bg-gray-200'}`}
